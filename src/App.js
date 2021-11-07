30d3fde8e272fc706e728b8e2b74adda18353d4e
Guillermo Alvarez <guillermo@icalialabs.com>
2021-11-07 11:25:58 -0700

    Connect to wallet and show test tweets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,117 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import { Tweet } from "react-twitter-widgets";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+
+          /**
+           *
+           * The solana object gives us a function that will allow us to connect
+           * directly with the user's wallet!
+           */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    window.addEventListener("load", async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const TEST_TWEETS = [
+    "841418541026877441",
+    "1457408745244332034",
+    "1442173206799044609",
+    "1457156043612459009",
+    "1457096407169609729",
+    "841418541026877441",
+    "1457408745244332034",
+    "1442173206799044609",
+    "1457156043612459009",
+    "1457096407169609729",
+  ];
+
+  const renderConnectedContainer = () => (
+    // <div className="connected-container">
+    <div className="gif-grid">
+      {TEST_TWEETS.map((tweetId, i) => (
+        <Tweet key={i} tweetId={tweetId} options={{ width: "400" }} />
+      ))}
+    </div>
+    // </div>
+  );
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+          <p className="header">
+            <img
+              alt="Twitter Logo"
+              style={{ height: "80px", width: "80px" }}
+              src={twitterLogo}
+            />
+            &nbsp; Tweet portal
           </p>
+          <p className="sub-text">
+            View your tweets collection in the metaverse ✨
+          </p>
+          {walletAddress
+            ? renderConnectedContainer()
+            : renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
